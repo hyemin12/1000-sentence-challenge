9a9a3f7efa26a51471e2345ee1ca6a3e79109a6d
hyemin12 <khnm31@naver.com>
2023-02-17 20:36:30 +0900

    '채점하기기능수정,50문장정리'

--- main.js
+++ main.js
@@ -86,11 +86,106 @@ const testArr = [
     해석: "방심하지마",
     정답: ["back"],
   },
+  { 문제: ["input", "what"], 해석: "있잖아", 정답: ["guess"] },
+  { 문제: ["I'll tell you", "input"], 해석: "내 말 들어봐", 정답: ["what"] },
+  { 문제: ["Give me a", "input"], 해석: "한번만 봐줘", 정답: ["break"] },
+  { 문제: ["input", "does it"], 해석: "살살해", 정답: ["easy"] },
+  { 문제: ["input", "me"], 해석: "비위 좀 맞춰줘", 정답: ["humor"] },
+  { 문제: ["Hear me", "input"], 해석: "끝까지 들어", 정답: ["out"] },
+  { 문제: ["Don't", "input", "me"], 해석: "말 끊지마", 정답: ["interrupt"] },
+  { 문제: ["input", "my words"], 해석: "내 말 잘 들어", 정답: ["mark"] },
+  {
+    문제: ["Are you", "input", "me?"],
+    해석: "무슨 말인지 알겠어?",
+    정답: ["following"],
+  },
+  {
+    문제: ["You", "input", "me?"],
+    해석: "알았어?",
+    정답: ["hear"],
+  },
+  {
+    문제: ["Don't you", "input", "?"],
+    해석: "모르겠어?",
+    정답: ["see"],
+  },
+  {
+    문제: ["input", "there,", "input", "that"],
+    해석: "나도 다 해봤어",
+    정답: ["been", "done"],
+  },
+  {
+    문제: ["I've learned my", "input"],
+    해석: "경험으로 배웠지",
+    정답: ["lesson"],
+  },
+  {
+    문제: ["I know him like a", "input"],
+    해석: "그 친구 내가 잘 알아",
+    정답: ["book"],
+  },
+  {
+    문제: ["He knows all the", "input"],
+    해석: "걔는 모르는거 없어",
+    정답: ["answers"],
+  },
+  {
+    문제: ["Who", "input", "?"],
+    해석: "혹시 알아?",
+    정답: ["knows"],
+  },
+  {
+    문제: ["Just", "input", "input"],
+    해석: "혹시 모르니까",
+    정답: ["in", "case"],
+  },
+  {
+    문제: ["I got your", "input"],
+    해석: "내가 있잖아",
+    정답: ["back"],
+  },
+  {
+    문제: ["You can", "input", "input", "me"],
+    해석: "나에게 맡겨",
+    정답: ["count", "on"],
+  },
+  {
+    문제: ["Follow my", "input"],
+    해석: "나만 따라와",
+    정답: ["lead"],
+  },
+  {
+    문제: ["I don't", "input"],
+    해석: "난 괜찮아",
+    정답: ["mind"],
+  },
+  {
+    문제: ["Money is no", "input"],
+    해석: "돈은 문제가 안돼",
+    정답: ["object"],
+  },
+  {
+    문제: ["It's just the", "input", "thing"],
+    해석: "술김에 하는 말이야",
+    정답: ["booze"],
+  },
+  {
+    문제: ["Get off my", "input"],
+    해석: "그만 성가시게해",
+    정답: ["back"],
+  },
+  {
+    문제: ["Stop", "input", "me"],
+    해석: "그만 좀 괴롭혀",
+    정답: ["bugging"],
+  },
 ];
 
 window.onload = function () {
   init();
 };
+
+// const
 
 const convertFormat = (number) => {
   return String(number + 1).padStart(4, "0");
@@ -138,10 +233,13 @@ const markingBtn = document.querySelector(".marking-button");
 let answer = document.getElementById("quiz-0001");
 
 const handleMarking = () => {
+  if (testContainer.classList.value === "marking") {
+    return;
+  }
+
   // 정답만 모은 정답목록 배열
   let answerArr = testArr.map(({ 정답 }) => [...정답]);
   let indexArr = document.querySelectorAll(".index");
-  console.log(indexArr);
 
   answerArr.map((answer, idx) => {
     let userAnswer = document.querySelectorAll(`.quiz-${convertFormat(idx)}`);
@@ -157,11 +255,24 @@ const handleMarking = () => {
           indexArr[idx].classList.add("o");
         })
       : userAnswer.forEach((input, i) => {
-          input.classList.add("wrong");
-          input.value = answer[i];
+          const correctAnswer = document.createElement("p");
+          correctAnswer.classList.add("wrong");
+          correctAnswer.innerText = answer[i];
+          input.before(correctAnswer);
+          input.remove();
           indexArr[idx].classList.add("x");
         });
   });
+  testContainer.classList.add("marking");
 };
 
 markingBtn.addEventListener("click", handleMarking);
+
+// 리셋 버튼
+const resetBtn = document.querySelector(".reset-button");
+
+const handleReset = () => {
+  testContainer.classList.value === "";
+  console.log(testContainer.classList);
+};
+resetBtn.addEventListener("click", handleReset);
